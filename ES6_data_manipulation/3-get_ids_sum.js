e490b392c929449415dfb3fb39078377e7caf31e
BradenE19 <6115@holbertonstudents.com>
2023-09-12 13:17:17 -0500

    lint fixes

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(listStudents) {
   let output = 0;
-  
+
   if (Array.isArray(listStudents)) {
     output = listStudents.reduce((sum, student) => sum + student.id, 0);
   }
