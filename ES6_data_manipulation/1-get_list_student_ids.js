452613c8175c27c3580981f2193c14d6fe413360
BradenE19 <6115@holbertonstudents.com>
2023-09-12 13:07:45 -0500

    dataman

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(listStudents) {
+    let output = [];
+
+    if (Array.isArray(listStudents)) {
+        output = listStudents.map((student) => student.id);
+    }
+    return output;
+}
